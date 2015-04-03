6fb069dc606d8ed740e716263711bc52b59d7b99
Olion <oleg70ua@yahoo.com>
2015-04-04 02:25:22 +0300

    [872] Regen NPC HP flag, except 9 NPCs

---
+++ World/Updates/Rel21/21000_07_Regenerate_NPC_Health.sql
@@ -0,0 +1,71 @@
+-- --------------------------------------------------------------------------------
+-- This is an attempt to create a full transactional update
+-- --------------------------------------------------------------------------------
+DROP PROCEDURE IF EXISTS `update_mangos`; 
+
+DELIMITER $$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `update_mangos`()
+BEGIN
+    DECLARE bRollback BOOL  DEFAULT FALSE ;
+    DECLARE CONTINUE HANDLER FOR SQLEXCEPTION SET `bRollback` = TRUE;
+
+  SET @cOldRev = 'required_21000_05_Script_refactoring'; 
+
+  -- Set the new revision string
+  SET @cNewRev = 'required_21000_05_Script_refactoring';
+
+  -- Set thisRevision to the column name of db_version in the currently selected database
+  SET @cThisRev := ((SELECT column_name FROM information_schema.`COLUMNS` WHERE table_name='db_version' AND table_schema=(SELECT DATABASE() AS thisDB FROM DUAL) AND column_name LIKE 'required%'));
+
+  -- Set friendly Version Text
+  SET @cThisVersion = 'MaNGOSZero Database Rev 21000_07';
+
+ 
+  -- Only Proceed if the old values match
+  IF @cThisRev = @cOldRev THEN
+    -- Make this all a single transaction
+    START TRANSACTION;
+
+    -- Apply the Version Change from Old Version to New Version
+    -- SET @query = CONCAT('ALTER TABLE db_version CHANGE COLUMN ',@cOldRev, ' ' ,@cNewRev,' bit;');
+    -- PREPARE stmt1 FROM @query;
+    -- EXECUTE stmt1;
+    -- DEALLOCATE PREPARE stmt1;
+    -- The Above block is required for making table changes
+    -- version
+    INSERT IGNORE INTO `db_version` SET `Version` = @cThisVersion;
+
+    -- If this is purely an incremental update, include this line
+    SET @cNewRev = @cThisVersion;
+
+    -- -- -- -- Normal Update / Insert / Delete statements will go here  -- -- -- -- --
+	
+
+-- Creature HP regen fix
+
+   UPDATE `creature_template` SET `RegenerateStats`=`RegenerateStats`|1 WHERE `entry` NOT IN (12423, 12427, 12428, 12429, 12430, 13020, 13148, 14484, 14485);
+    
+    -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+    
+    -- If we get here ok, commit the changes
+    IF bRollback = TRUE THEN
+      ROLLBACK;
+      SELECT '* UPDATE FAILED *' AS 'Status',@cThisRev AS 'DB is on Version';
+    ELSE
+      COMMIT;
+      SELECT '* UPDATE COMPLETE *' AS 'Status',@cNewRev AS 'DB is now on Version';
+    END IF;
+  ELSE
+    SELECT '* UPDATE SKIPPED *' AS 'Status',@cOldRev AS 'Required Version',@cThisRev AS 'Found Version';
+  END IF;
+
+END $$
+
+DELIMITER ;
+
+-- Execute the procedure
+CALL update_mangos();
+
+-- Drop the procedure
+DROP PROCEDURE IF EXISTS `update_mangos`;
