463400b71621d7a9bdf8df7fcb63e8279d16c509
Antz <billy1arm@users.noreply.github.com>
2015-05-28 23:48:00 +0100

    Added missing Tables

--- World/Setup/mangosdLoadDB.sql
+++ World/Setup/mangosdLoadDB.sql
@@ -5978,6 +5978,52 @@ CREATE TABLE `pool_template` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Table structure for table `pvpstats_battlegrounds`
+--
+
+DROP TABLE IF EXISTS `pvpstats_battlegrounds`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `pvpstats_battlegrounds` (
+  `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `winner_team` TINYINT NOT NULL,
+  `bracket_id` TINYINT UNSIGNED NOT NULL,
+  `type` TINYINT UNSIGNED NOT NULL,
+  `date` DATETIME NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `pvpstats_players`
+--
+
+DROP TABLE IF EXISTS `pvpstats_players`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `pvpstats_players` (
+  `battleground_id` BIGINT UNSIGNED NOT NULL,
+  `player_guid` INT UNSIGNED NOT NULL,
+  `score_killing_blows` MEDIUMINT UNSIGNED NOT NULL,
+  `score_deaths` MEDIUMINT UNSIGNED NOT NULL,
+  `score_honorable_kills` MEDIUMINT UNSIGNED NOT NULL,
+  `score_bonus_honor` MEDIUMINT UNSIGNED NOT NULL,
+  `score_damage_done` MEDIUMINT UNSIGNED NOT NULL,
+  `score_healing_done` MEDIUMINT UNSIGNED NOT NULL,
+  `attr_1` MEDIUMINT UNSIGNED NOT NULL DEFAULT 0,
+  `attr_2` MEDIUMINT UNSIGNED NOT NULL DEFAULT 0,
+  `attr_3` MEDIUMINT UNSIGNED NOT NULL DEFAULT 0,
+  `attr_4` MEDIUMINT UNSIGNED NOT NULL DEFAULT 0,
+  `attr_5` MEDIUMINT UNSIGNED NOT NULL DEFAULT 0,
+  PRIMARY KEY (`battleground_id`, `player_guid`)
+) ENGINE=InnoDB;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+
+
+
+--
 -- Table structure for table `quest_template`
 --
 
