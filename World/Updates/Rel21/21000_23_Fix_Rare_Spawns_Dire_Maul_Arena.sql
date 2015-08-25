e10886d19806d77888c79abab4f9c258c321abd4
H0zen <H0zen@users.noreply.github.com>
2015-08-25 18:13:41 +0300

    Fix previous commit. Some logic errors.

--- World/Updates/Rel21/21000_23_Fix_Rare_Spawns_Dire_Maul_Arena.sql
+++ World/Updates/Rel21/21000_23_Fix_Rare_Spawns_Dire_Maul_Arena.sql
@@ -13,6 +13,9 @@ BEGIN
   -- old revision
   SET @cOldRev := '21.1.0';
 
+  -- Set the new revision string
+  SET @cNewRev := '21.1.0';
+
   -- Set thisRev to the one in db_version in the currently selected database
   SELECT CONCAT(`version`, ".",`structure`, ".", `content`) INTO @cThisRev FROM `db_version`;
 
@@ -27,17 +30,14 @@ BEGIN
     START TRANSACTION;
 
     -- version
-    TRUNCATE `db_version`;
-    INSERT IGNORE INTO `db_version` VALUES (21, 1, 1, @cDescription, @cComment);
-
-    -- Set the new revision string
-    SET @cNewRev := @cThisRev;
+    -- TRUNCATE `db_version`;
+    -- INSERT IGNORE INTO `db_version` VALUES (21, 1, 1, @cDescription, @cComment);
 
     -- -- -- -- Normal Update / Insert / Delete statements will go here  -- -- -- -- --
   
   	-- Correct some attributes & make 3 rare elites spawn in Dire Maul Arena 
         REPLACE INTO `creature_template` VALUES (11447, 'Mushgog', NULL, 60, 60, 14382, 0, 14, 14, 0, 0, 4, 1, 1, 0, 0, 0, 0, 0, 0, 3.58, 1.14286, 1, 2, 20, 0, 1, 1, 1, 1, 61040, 61040, 0, 0, 571, 737, 61.732, 84.8815, 0, 302, 100, 1150, 1265, 0, 168, 838, 0, 0, 0, 0, 0, 8395857, 0, 0, 0, 0, 30, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'EventAI');
-        REPLACE INTO `creature_template` VALUES (11497, 'The Razza', NULL, 60, 60, 10807, 0, 14, 14, 0, 0, 0, 4, 3, 0, 0, 0, 0, 0, 0, 1.79, 1.14286, 2, 2, 30, 10, 1, 1, 1, 1, 73260, 73260, 24340, 24340, 571, 737, 61.732, 84.8815, 0, 302, 100, 1150, 1265, 0, 168, 838, 0, 0, 0, 0, 0, 8395857, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'EventAI');
+        REPLACE INTO `creature_template` VALUES (11497, 'The Razza', NULL, 60, 60, 10807, 0, 14, 14, 0, 0, 1, 4, 3, 0, 0, 0, 0, 0, 0, 1.79, 1.14286, 2, 2, 30, 10, 1, 1, 1, 1, 73260, 73260, 24340, 24340, 571, 737, 61.732, 84.8815, 0, 302, 100, 1150, 1265, 0, 168, 838, 0, 0, 0, 0, 0, 8395857, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'EventAI');
         REPLACE INTO `creature_template` VALUES (11498, 'Skarr the Unbreakable', NULL, 58, 58, 10169, 0, 14, 14, 0, 0, 7, 1, 1, 0, 0, 0, 0, 0, 0, 3.58, 1.14286, 1, 2, 20, 0, 1, 1, 1, 1, 57420, 57420, 0, 0, 380, 491, 61.732, 84.8815, 0, 201, 100, 1150, 1265, 0, 168, 838, 0, 0, 0, 0, 0, 8395857, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'EventAI');
 
         INSERT INTO `creature` VALUES (590013, 11447, 1, 0, 0, -3755.33, 1094.43, 131.969, 3.11817, 21600, 10, 0, 61040, 0, 0, 1);
