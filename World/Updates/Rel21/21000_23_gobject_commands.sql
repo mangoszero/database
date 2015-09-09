96a710406b4c96c2982587dad6f50ca3f535c3e8
Antz <Antz@getMaNGOS.co.uk>
2015-09-10 00:01:44 +0100

    Applied new update process

--- World/Updates/Rel21/21000_23_gobject_commands.sql
+++
@@ -1,6 +0,0 @@
--- strings for new gobj commands: state, lootstate
-
-DELETE FROM `mangos_string` WHERE `entry` IN (573,574);
-INSERT INTO `mangos_string` (`entry`,`content_default`) VALUES
- (573,'The lootstate of GO lowGuid %u is %u.'),
- (574,'The GO state of GO lowGuid %u is %u.');
