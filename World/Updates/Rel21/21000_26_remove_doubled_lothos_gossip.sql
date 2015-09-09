916fde234d03f013958c5636459b4f31df072ce6
Antz <Antz@getMaNGOS.co.uk>
2015-09-10 00:18:55 +0100

    Applied new update process

--- World/Updates/Rel21/21000_26_remove_doubled_lothos_gossip.sql
+++
@@ -1,6 +0,0 @@
--- delete doubled gossip_menu_option for the teleport to molten core
-
-DELETE FROM `gossip_menu_option` WHERE `option_text` LIKE 'Teleport me to the Molten Core, Lothos.';
-UPDATE `gossip_menu_option` SET `id` = 0 WHERE `option_text` LIKE 'Transport me to the Molten Core, Lothos.';
-
-INSERT INTO `db_version` VALUES ('21', '1', '4', 'remove_doubled_lothos_gossip', '');
