f5999e4cc8db1bb45bd29b482028174c74a9203f
Antz <Antz@getMaNGOS.co.uk>
2015-09-10 00:10:16 +0100

    Applied new update process

--- World/Updates/Rel21/21000_24_delete_doubled_mc_rune_spawns.sql
+++
@@ -1,6 +0,0 @@
--- delete doubled molten core rune spawns
-
-DELETE FROM `gameobject` WHERE `guid` = 43162;
-DELETE FROM `gameobject` WHERE `guid` = 43164;
-
-INSERT INTO `db_version` VALUES ('21', '1', '2', 'remove_doubled_runes', '');
