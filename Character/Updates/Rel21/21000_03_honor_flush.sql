a4280728275cb5ddac3eb771e57ea5792a4aaa24
Antz <Antz@getMaNGOS.co.uk>
2015-09-10 00:38:17 +0100

    Applied new update process

--- Character/Updates/Rel21/21000_03_honor_flush.sql
+++
@@ -1,4 +0,0 @@
--- mark the already counted rows in the hk table
-ALTER TABLE `character_honor_cp` ADD COLUMN `used` TINYINT(1) NOT NULL DEFAULT 0 COMMENT '=1 if already counted in rank points';
--- change version
-INSERT INTO `db_version` (`version`,`structure`,`content`,`description`) VALUES (21,2,0,'honor_counting');
