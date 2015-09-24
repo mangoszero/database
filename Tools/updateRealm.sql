0b650b54ded04c08e3be450f666d8025024c4642
Antz <Antz@getMaNGOS.co.uk>
2015-09-25 00:21:33 +0100

    make SQL like linux again. Thanks to Faded for pointing

--- Tools/updateRealm.sql
+++ Tools/updateRealm.sql
@@ -1,15 +1,15 @@
     -- Get the Latest RealmID
     SET @RealmCount := (SELECT AUTO_INCREMENT FROM information_schema.tables WHERE table_name='realmlist' AND table_schema=(SELECT DATABASE()));
-    SET @RealmName = 'MaNGOS Zero Server #';    
-    
-    -- Insert a new unique Row    
-    INSERT INTO Realmlist SET `NAME`=CONCAT(@RealmName,CAST(@RealmCount AS CHAR));
+    SET @RealmName = 'MaNGOS Zero Server #';
+
+    -- Insert a new unique Row
+    INSERT INTO realmlist SET `NAME`=CONCAT(@RealmName,CAST(@RealmCount AS CHAR));
 
     -- Get the id of the inserted row
-    SET @NewRealm := (SELECT MAX(id) FROM Realmlist);
+    SET @NewRealm := (SELECT MAX(id) FROM realmlist);
 
     -- Update the information
-    UPDATE Realmlist SET realmbuilds='5875 6005 6141' WHERE id=@NewRealm;
+    UPDATE realmlist SET realmbuilds='5875 6005 6141' WHERE id=@NewRealm;
 
-    -- Display the Realm Info    
+    -- Display the Realm Info
     SELECT CONCAT('Realm Name: ',@RealmName,CAST(@RealmCount AS CHAR),'  Realm ID: ',CAST(@NewRealm AS CHAR)) AS 'Realm Information';
