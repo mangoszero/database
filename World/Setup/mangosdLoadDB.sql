f825c99f35ab8de1f10b7927363a0337ba5cabda
Antz <billy1arm@users.noreply.github.com>
2016-02-25 14:53:28 +0000

    [Fix] Added missing table to mangosLoadDB.sql

--- World/Setup/mangosdLoadDB.sql
+++ World/Setup/mangosdLoadDB.sql
@@ -1110,6 +1110,23 @@ CREATE TABLE `dbscripts_on_spell` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Table structure for table `disables`
+--
+
+DROP TABLE IF EXISTS `disables`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `disables` (
+ `sourceType` int(10) unsigned NOT NULL COMMENT 'Type of disable, see enum DisableType',
+ `entry` int(10) unsigned NOT NULL COMMENT 'Main entry for disable, type-specific',
+ `flags` tinyint(3) unsigned NOT NULL DEFAULT 0 COMMENT 'Flag bitfield, type-specific',
+ `data` int(10) unsigned NOT NULL DEFAULT 0 COMMENT 'Additional data, type-specific',
+ `comment` varchar(255) DEFAULT NULL COMMENT 'Description of disable cause',
+ PRIMARY KEY(`sourceType`,`entry`,`data`)
+) ENGINE=MyISAM COMMENT='Mangos disables system';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Table structure for table `disenchant_loot_template`
 --
 
