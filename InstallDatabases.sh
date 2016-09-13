cdc8c395cebf5ae6eed013e874eafbb66b182d12
Warkdev <cedric_lesi@hotmail.com>
2016-09-13 23:14:43 +0200

    Adding support to dump database configuration with -d parameter on In… (#66)
    * Adding support to dump database configuration with -d parameter on InstallDatabases.sh
    
    * Fixing a bug that made the InstallDatabases.sh always dumping the database information
    
    * Updating InstallDatabases.sh to not update the database by default. (Only -u option will update the database)
    
    * Fixing InstallDatabases.sh to perform only necessary updates (if available - as explained by Antz !)

--- InstallDatabases.sh
+++ InstallDatabases.sh
@@ -7,6 +7,7 @@
 
 RELEASE="Rel21"
 UPDATES="Rel20_to_BaseRel21_Updates"
+DUMP="NO"
 
 createcharDB="YES"
 createworldDB="YES"
@@ -33,9 +34,10 @@ rdb_def="realmd"
 
 printHelp()
 {
-	printf "Usage: $0 [-s] [-u] [-h]\n"
+	printf "Usage: $0 [-s] [-u] [-d] [-h]\n"
 	printf "\t-s: Run this script in silent mode, only prompt for the database information\n"
 	printf "\t-u: Run only the updates of the database\n"
+	printf "\t-d: Dump the database configuration into the home directory of the user\n"
 	printf "\t-h: Display this help\n"
 }
 
@@ -96,7 +98,7 @@ loadCharDB()
 updateCharDB()
 {
 	printf "Updating data into the character database ${cdb}\n"
-	for file in $(ls Character/Updates/${RELEASE}/${UPDATES}/*.sql | tr ' ' '|' | tr '\n' ' ')
+	for file in $(ls Character/Updates/*.sql | tr ' ' '|' | tr '\n' ' ')
 	do
 		file=$(echo ${file} | tr '|' ' ')
 		printf "Applying update ${file}\n"
@@ -138,7 +140,7 @@ populateWorldDB()
 updateWorldDB()
 {
 	printf "Updating data into the World database ${wdb}\n"
-	for file in $(ls World/Updates/${RELEASE}/${UPDATES}/*.sql | tr ' ' '|' | tr '\n' ' ')
+	for file in $(ls World/Updates/*.sql | tr ' ' '|' | tr '\n' ' ')
 	do
 		file=$(echo ${file} | tr '|' ' ')
 		printf "Applying update ${file}\n"
@@ -164,7 +166,7 @@ loadRealmDB()
 updateRealmDB()
 {
 	printf "Updating data into the Realm database ${rdb}\n"
-	for file in $(ls Realm/Updates/${RELEASE}/${UPDATES}/*.sql | tr ' ' '|' | tr '\n' ' ')
+	for file in $(ls Realm/Updates/*.sql | tr ' ' '|' | tr '\n' ' ')
 	do
 		file=$(echo ${file} | tr '|' ' ')
 		printf "Applying update ${file}\n"
@@ -196,6 +198,9 @@ while getopts "suh" o; do
 			loadrealmDB="NO"
 			dbType="EMPTY"
 			printf "You selected update only\n"
+			;;
+		d)
+			DUMP="YES"
 			;;
 		h)
 			printHelp
@@ -307,6 +312,10 @@ read port
 port=${port:-${port_def}}
 printf "What is your MySQL password ?\t [], "
 mysql_config_editor set --login-path=local --host=${svr} --port=${port} --user=${user} --password --skip-warn
+if [ "${DUMP}" = "YES" ]; then
+	printf "Enter it again \t[]: "
+	read pass
+fi
 printf "What is your Character database name ?\t[${cdb_def}]: "
 read cdb
 cdb=${cdb:-${cdb_def}}
@@ -343,6 +352,13 @@ fi
 
 if [ "${addRealmList}" = "YES" ]; then
 	addRealmList
+fi
+
+if [ "${DUMP}" = "YES" ]; then
+	printf "Dumping database information...\n"
+	echo "${svr};${port};${user};${pass};${rdb}" > ~/db.conf
+	echo "${svr};${port};${user};${pass};${wdb}" >> ~/db.conf
+	echo "${svr};${port};${user};${pass};${cdb}" >> ~/db.conf
 fi
 
 printBanner
