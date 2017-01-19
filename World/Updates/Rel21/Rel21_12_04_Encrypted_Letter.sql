b507a65a010ee2fd24c532c95f1721bfee4087e6
Antz <billy1arm@users.noreply.github.com>
2017-01-19 09:25:08 +0000

    Update and rename Rel21_12_02_Encrypted_Letter.sql to Rel21_12_04_Encrypted_Letter.sql

--- World/Updates/Rel21/Rel21_12_04_Encrypted_Letter.sql
+++ World/Updates/Rel21/Rel21_12_04_Encrypted_Letter.sql
@@ -27,12 +27,12 @@ BEGIN
     -- Expected Values
     SET @cOldVersion = '21'; 
     SET @cOldStructure = '12'; 
-    SET @cOldContent = '1';
+    SET @cOldContent = '3';
 
     -- New Values
     SET @cNewVersion = '21';
     SET @cNewStructure = '12';
-    SET @cNewContent = '2';
+    SET @cNewContent = '4';
                             -- DESCRIPTION IS 30 Characters MAX    
     SET @cNewDescription = 'Encrypted_Letter';
 
