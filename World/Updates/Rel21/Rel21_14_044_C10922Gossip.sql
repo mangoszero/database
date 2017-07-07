8bbf79216f2bd80ff5e421fcf8d46b6ad1c73cb6
bdebaere <brechtdebaere@yahoo.com>
2017-07-07 22:36:54 +0200

    Update Rel21_14_044_C10922Gossip.sql
    Fixed Mangos db structure

--- World/Updates/Rel21/Rel21_14_044_C10922Gossip.sql
+++ World/Updates/Rel21/Rel21_14_044_C10922Gossip.sql
@@ -61,7 +61,7 @@ BEGIN
 UPDATE
 	`npc_text`
 SET
-	`text0_1` = 'I welcome you, $N. And the Emerald Circle welcomes you.$B$BHopefully together we can help rejuvenate and protect this once-mighty forest. Many forces seek to destroy it from without... and within.$B$BDo not be persuaded by the power here--it will only corrupt you, like it has this entire land.'
+	`text0_0` = 'I welcome you, $N. And the Emerald Circle welcomes you.$B$BHopefully together we can help rejuvenate and protect this once-mighty forest. Many forces seek to destroy it from without... and within.$B$BDo not be persuaded by the power here--it will only corrupt you, like it has this entire land.'
 WHERE
 	`ID` = 3807;
 
