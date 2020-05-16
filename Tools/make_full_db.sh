ee98341aa091793d9fa0e4df51ef31513238aa3f
Antz <billy1arm@users.noreply.github.com>
2020-05-16 09:07:55 +0100

    Minor banner update

--- Tools/make_full_db.sh
+++ Tools/make_full_db.sh
@@ -1,7 +1,6 @@
 cat <<EOF > ../full_db.sql
 --
--- Copyright (C) 2005-2013 MaNGOS <http://getmangos.com/>
--- Copyright (C) 2009-2012 MaNGOSTwo <https://github.com/mangostwo>
+-- Copyright (C) 2005-2020 MaNGOS <http://getmangos.eu/>
 --
 -- This program is free software; you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
